a8cc24de3635ce2410f016d34d5d053841628883
Adrian-Ferreira <adrian.zoeferreira@gmail.com>
2019-11-29 14:49:08 -0300

    JS

---
+++ JavaScript/Checklist.js
@@ -0,0 +1,12 @@
+function inserirNovo(){
+
+var xuxa= document.getElementById("xuxa").value;
+var lego= "<div class='input-group-prepend'><div class='input-group-text'>  <input type='checkbox' aria-label='Checkbox for following text input'></div></div><input type='text'style='width: 500px; height: 70px; class='form-control' value="+xuxa+" aria-label='Text input with checkbox'></div>"
+
+document.getElementById("felipe").innerHTML += lego;
+
+
+
+
+
+}
